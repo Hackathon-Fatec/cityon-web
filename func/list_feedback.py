92d8661d14c9d6101251dc000a091f36effd8b04
Harlocchi <gustaalves1701@gmail.com>
2023-08-19 01:54:32 -0300

    atualização tela de listagem

--- func/list_feedback.py
+++ func/list_feedback.py
@@ -31,19 +31,35 @@ def list_feedback():
 
                 col1, col2 = st.columns(2)
                 with col1:
-                    st.markdown("# Foto")
+                    st.markdown("### Foto")
                     st.image(imagem, use_column_width=True)
                 with col2:
-                    st.markdown("# Informações")
-                    st.write(f"Nome: {local[1]}")
-                    st.write(f"Cidade: {local[3]}")
-                    st.write(f"Endereço: {local[4]}")
-                    st.write(f"Descrição: {local[5]}")
-                    st.write(f"Data: {local[7]}")
+                    st.markdown("### Descrição")
                     if local[6] == "Positive":
-                        st.write(f"Sentimento: {local[6]} 😀")
-                    else:
-                        st.write(f"Sentimento: {local[6]} 😔")
+                        stment = f"Sentimento: {local[6]} 😀"
+                    else: 
+                        stment = f"{local[6]} 😔"
+                    texto_description = (
+                          f'<p style = "color: #808080"><span style="color: white;">Nome: </span> {local[1]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Cidade: </span> {local[3]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Endereço: </span> {local[4]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Descrição: </span> {local[5]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Date: </span> {local[7]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Sentimento: </span> {stment}</p>'
+                    )
+                    st.markdown(texto_description, unsafe_allow_html=True)
+
+                space = '<p style = "color: #4a28ea" > ______________________________________________________________________________________</p>'
+                st.markdown(space, unsafe_allow_html=True)
+                    
+
+                    #st.markdown("# Informações")
+                    #st.write(f"Nome: {local[1]}")
+                    #st.write(f"Cidade: {local[3]}")
+                    #st.write(f"Endereço: {local[4]}")
+                    #st.write(f"Descrição: {local[5]}")
+                    #st.write(f"Data: {local[7]}")
+                   
 
         else:
             st.warning('Nenhum dado encontrado!', icon="⚠️")
