39d212cb7a271be12f2ad5fc16698e32247326b8
Joao Assalim <joassalim@gmail.com>
2023-08-19 14:05:41 -0300

    model

--- func/list_feedback.py
+++ func/list_feedback.py
@@ -3,8 +3,6 @@ from db import get_itens, get_filtred_cities
 from get_cities import get_cities
 from PIL import Image
 import io
-import base64
-from card_layout import exibir_feedback
 
 cities = get_cities()
 
@@ -17,6 +15,8 @@ def list_feedback():
     with sentiment:
         sentiment_choosed = st.selectbox('Escolha o sentimento do comentario', ["Positivo", "Negativo", "Todos"])
 
+    st.write("Content outside the cards.")
+
     if st.button("Buscar"):
         filtred_cities = []
         if sentiment_choosed == "Todos":
@@ -25,14 +25,41 @@ def list_feedback():
             filtred_cities = get_filtred_cities(city_choosed, "Positive" if sentiment_choosed == "Positivo" else "Negative")
 
         if len(filtred_cities) > 0:
-            for i in filtred_cities:
-                bytes_io = io.BytesIO(i[2])
+            for local in filtred_cities:
+                bytes_io = io.BytesIO(local[2])
                 imagem = Image.open(bytes_io)
-                image_base64 = base64.b64encode(bytes_io.getvalue()).decode()
 
-                html_code = f'<img src="data:image/jpeg;base64,{image_base64}" alt="Imagem" width="150" style="margin-right: 16px;" />'
-                exibir_feedback(html_code, i[1], i[5], i[6])
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.markdown("### Foto")
+                    st.image(imagem, use_column_width=True)
+                with col2:
+                    st.markdown("### Descrição")
+                    if local[6] == "Positive":
+                        stment = f"Sentimento: {local[6]} 😀"
+                    else: 
+                        stment = f"{local[6]} 😔"
+                    texto_description = (
+                          f'<p style = "color: #808080"><span style="color: white;">Nome: </span> {local[1]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Cidade: </span> {local[3]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Endereço: </span> {local[4]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Descrição: </span> {local[5]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Date: </span> {local[7]}</p>'
+                          f'<p style = "color: #808080" ><span style="color: white;">Sentimento: </span> {stment}</p>'
+                    )
+                    st.markdown(texto_description, unsafe_allow_html=True)
+
+                space = '<p style = "color: #4a28ea" > ______________________________________________________________________________________</p>'
+                st.markdown(space, unsafe_allow_html=True)
+                    
+
+                    #st.markdown("# Informações")
+                    #st.write(f"Nome: {local[1]}")
+                    #st.write(f"Cidade: {local[3]}")
+                    #st.write(f"Endereço: {local[4]}")
+                    #st.write(f"Descrição: {local[5]}")
+                    #st.write(f"Data: {local[7]}")
+                   
+
         else:
             st.warning('Nenhum dado encontrado!', icon="⚠️")
-
-list_feedback()
---
+++ func/predict.py
@@ -0,0 +1,23 @@
+import joblib
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+import string
+
+lemmatizer = WordNetLemmatizer()
+def predict(opinion):
+    model_filename = "../models/sentiment_analyser_model.pkl"
+    vect_filename = "../models/sentiment_analyser_vect.pkl"
+
+    loaded_model = joblib.load(model_filename)
+    loaded_vectorizer = joblib.load(vect_filename)
+
+    stop_words = nltk.corpus.stopwords.words('portuguese')
+
+    new_data = word_tokenize(opinion.lower())
+    new_opinion = " ".join([lemmatizer.lemmatize(str(token)) for token in new_data if not str(token) in stop_words and not token in string.punctuation and "@" not in token and "http" not in token and len(token) > 1 and not token.isdigit()])
+
+    new_data_transformed = loaded_vectorizer.transform([new_opinion])
+    predictions = loaded_model.predict(new_data_transformed)
+
+    return "Positive" if predictions[0] == 1 else "Negative"
